196d5328671fe5c40b7460afee899426ec74a776
Lucas <biblion84@free.fr>
2017-05-05 23:00:31 +0200

    Refactorisation

--- app.js
+++ app.js
@@ -1,3 +1,74 @@
+const MAX_TABLE_ROW = 8;
+
+let numero_element;
+let elements = document.getElementsByClassName("grid-snap");
+let coordonnes = new Array(elements.length);
+
+interact(".grid-snap") // librairie interact.js
+    .draggable({
+
+        snap: {
+            targets: [interact.createSnapGrid({x: 5, y: 5})],
+            range: Infinity,
+            relativePoints: [{x: 0, y: 0}]
+        },
+        inertia: false,
+        restrict: {
+            restriction: document.getElementsByClassName("grid-snap").parentNode,
+            elementRect: {top: 0, left: 0, bottom: 1, right: 1},
+            endOnly: true
+        }
+    })
+    .on('dragmove', function (event) {
+        numero_element = event.target.attributes.getNamedItem("number").nodeValue;
+        coordonnes[numero_element][0] += event.dx;
+        coordonnes[numero_element][1] += event.dy;
+
+        event.target.style.transform = 'translate(' + coordonnes[numero_element][0] + 'px, ' + coordonnes[numero_element][1] + 'px)';
+        if (event.target.attributes.getNamedItem("rotated").nodeValue)
+            event.target.style.webkitTransform += 'rotate(' + event.target.attributes.getNamedItem("rotated").nodeValue + 'deg)';
+    })
+    .on('doubletap', function (event) {
+        let rotation = ( parseInt(event.target.attributes.getNamedItem("rotated").nodeValue) + 45) % 360;
+        event.target.setAttribute("rotated", rotation);
+        event.target.style.webkitTransform += 'rotate(' + 45 + 'deg)';
+    });
+
+function addTable(iteration) {
+    let grid = document.getElementById("table");
+    grid = grid.lastElementChild.lastElementChild;
+
+    let td = document.createElement("td");
+    let table = document.createElement("div");
+    table.setAttribute("class", "grid-snap");
+
+    td.appendChild(table);
+
+    if (grid.childElementCount > MAX_TABLE_ROW) {
+        grid = grid.parentNode;
+        let tr = document.createElement("tr");
+        tr.appendChild(td);
+        grid.appendChild(tr);
+    } else {
+        grid.appendChild(td);
+    }
+
+    elements = document.getElementsByClassName("grid-snap");
+    let lst_tb = elements.length - 1;
+    coordonnes[lst_tb] = [];
+    coordonnes[lst_tb][0] = 0;
+    coordonnes[lst_tb][1] = 0;
+    elements[lst_tb].setAttribute("number", lst_tb);
+    elements[lst_tb].setAttribute("rotated", "0");
+    elements[lst_tb].innerHTML = "&nbsp".repeat(5) + elements.length;
+
+    if (iteration > 0) {
+        addTable(iteration - 1);
+    }
+}
+
+
+
 /*
 
 
@@ -41,113 +112,6 @@
 
 
  */
-
-var element;
-var NB_table = 0;
-var numero_element;
-var elements = document.getElementsByClassName("grid-snap");
-var i;
-var coordonnes = new Array(elements.length);
-var tables = new Array(elements.length);
-for (i = 0; i < elements.length; i++) {
-    coordonnes[i] = new Array(2);
-    coordonnes[i][0] = 0;
-    coordonnes[i][1] = 0;
-    elements[i].setAttribute("number", i);
-    elements[i].setAttribute("rotated", 0);
-    elements[i].innerHTML ="&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" +  (i + 1); //lol
-}
-
-
-
-interact(".grid-snap")
-    .draggable({
-
-        snap: {
-            targets: [interact.createSnapGrid({x: 5, y: 5})],
-            range: Infinity,
-            relativePoints: [{x: 0, y: 0}]
-        },
-        inertia: false,
-        restrict: {
-            restriction: document.getElementsByClassName("grid-snap").parentNode,
-            elementRect: {top: 0, left: 0, bottom: 1, right: 1},
-            endOnly: true
-        }
-    })
-    .on('dragmove', function (event) {
-        numero_element = event.target.attributes.getNamedItem("number").nodeValue;
-        coordonnes[numero_element][0] += event.dx;
-        coordonnes[numero_element][1] += event.dy;
-
-        event.target.style.transform = 'translate(' + coordonnes[numero_element][0] + 'px, ' + coordonnes[numero_element][1] + 'px)';
-        if (event.target.attributes.getNamedItem("rotated").nodeValue)
-            event.target.style.webkitTransform += 'rotate(' + event.target.attributes.getNamedItem("rotated").nodeValue + 'deg)';
-    })
-    .on('doubletap', function (event) {
-        var change;
-        console.log(event.target.attributes.getNamedItem("rotated").nodeValue);
-        let rotation = ( parseInt(event.target.attributes.getNamedItem("rotated").nodeValue) + 45) % 360;
-        event.target.setAttribute("rotated", rotation);
-
-        // numero_element = event.target.attributes.getNamedItem("number").nodeValue;
-        event.target.style.webkitTransform += 'rotate(' + 45 + 'deg)';
-        console.log(rotation);
-
-    });
-
-function addTable() {
-    let grid = document.getElementById("table");
-    grid = grid.lastElementChild.lastElementChild;
-
-    console.log(grid.childElementCount);
-    let td = document.createElement("td");
-    let table = document.createElement("div");
-    table.setAttribute("class", "grid-snap");
-
-    td.appendChild(table);
-
-    if (grid.childElementCount > 5){
-        grid = grid.parentNode;
-        let tr = document.createElement("tr");
-        tr.appendChild(td);
-        grid.appendChild(tr);
-    } else
-        grid.appendChild(td);
-    elements = document.getElementsByClassName("grid-snap");
-
-    coordonnes[elements.length - 1] = new Array(2);
-    coordonnes[elements.length - 1][0] = 0;
-    coordonnes[elements.length - 1][1] = 0;
-    elements[elements.length - 1].setAttribute("number", elements.length - 1);
-    elements[elements.length - 1].setAttribute("rotated", 0);
-    elements[elements.length - 1].innerHTML = elements.length;
-
-}
-
-/*
- function Table(element) {
- this.element = element;
- this.rotated = false;
- this.x = 0;
- this.y = 0;
- this.nb =NB_table;
- NB_table = NB_table +  1;
- this.rotate = function () {
- if (!this.rotated)
- this.element.style.webkitTransform += 'rotate('+90+'deg)';
- else
- this.element.style.webkitTransform =  'translate(' + x + 'px, ' + y + 'px)';
- this.rotated =! this.rotated;
- }
- this.moving = function () {
- this.element.style.webkitTransform ='translate(' + x + 'px, ' + y + 'px)'
- if (this.rotated)
- this.element.style.webkitTransform +=' rotate('+90+'deg)';
- }
- }*/
-
-
 
 
 
